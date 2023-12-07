0f7dd66ff77c08717a1761eb793a58f74e61cc98
Viggo Büller <viggo.buller@learnet.se>
2023-12-07 14:52:51 +0100

    Commit 19
    damage blink

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,15 +5,19 @@ using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    [Header("HealthBar")]
-    [SerializeField] Image healthBar;
-    [SerializeField] Image healthBarBG;
-    [Space]
-    [SerializeField] float healthBarSlideSpeed = 0.005f;
     [Header("Player")]
+    [SerializeField] SpriteRenderer player;
     [SerializeField] GameObject deathEffect;
     [SerializeField] float maxHealth = 100f;
-    [SerializeField] float invisDuration = 0.5f;
+
+    [Header("HealthBar")]
+    [SerializeField, Tooltip("Health Left")] Image healthBar;
+    [SerializeField, Tooltip("Health Lost")] Image lostHealthBar;
+    [SerializeField] float slideSpeed = 0.005f;
+
+    [Header("Invisible")]
+    [SerializeField] float invisDuration = 1f;
+    [SerializeField] float blinkRate = 0.1f;
 
     float currentHealth;
     bool isInvis = false;
@@ -24,19 +28,20 @@ public class PlayerHealth : MonoBehaviour
     private void Start()
     {
         cameraShake = FindObjectOfType<CameraShake>();
-
+        
         currentHealth = maxHealth;
     }
 
     private void Update()
     {
-        healthBarBG.fillAmount = Mathf.Lerp(healthBarBG.fillAmount, healthBar.fillAmount - 0.01f, healthBarSlideSpeed);
+        HealthBarUpdate();
+        StartCoroutine(Blinking());
 
         //För test
         if (Input.GetKeyDown(KeyCode.Escape)) 
         { 
             PlayerDamage(20);
-            StartCoroutine(FindObjectOfType<PlayerMove>().Knockback(new Vector2(50f, 2f), 1, 0.2f));
+            StartCoroutine(FindObjectOfType<PlayerMove>().Knockback(new Vector2(0f, 2f), 0f, 0.2f));
         }
     }
 
@@ -56,10 +61,13 @@ public class PlayerHealth : MonoBehaviour
             }
         }
     }
+    private void HealthBarUpdate()
+    {
+        lostHealthBar.fillAmount = Mathf.Lerp(lostHealthBar.fillAmount, healthBar.fillAmount - 0.01f, slideSpeed);
+    }
 
     private void Die()
     {
-        //Instantiate(deathEffect, transform.position, Quaternion.identity);
         isDead = true;
         Debug.Log("Player is dead");
     }
@@ -69,5 +77,16 @@ public class PlayerHealth : MonoBehaviour
         isInvis = true;
         yield return new WaitForSeconds(invisDuration);
         isInvis = false;
+    }
+
+    private IEnumerator Blinking()
+    {
+        while (isInvis)
+        {
+            player.enabled = false;
+            yield return new WaitForSeconds(blinkRate);
+            player.enabled = true;
+            yield return new WaitForSeconds(blinkRate);
+        }
     }
 }
